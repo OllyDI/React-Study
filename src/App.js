7ac2592b940065ff8b5b4da6d5762dbff6b0447e
OllyDI <choiseungho11@hanmail.net>
2024-11-07 21:19:09 +0900

    24.11.07

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
 
 function Header(props) {
   return (
     <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(e) => {
+        e.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   );
 }
+
 
 function Nav(props) {
   const lis = [];
@@ -15,7 +30,10 @@ function Nav(props) {
   props.topics.map((data, index) => (
     lis.push(
     <li key={data.id}>
-      <a href={'/read/' + data.id}>{data.title}</a>
+      <a id={data.id} href={'/read/' + data.id} onClick={(e) => {
+        e.preventDefault();
+        props.onChangeMode(data.id);
+      }}>{data.title}</a>
     </li>
     )))
 
@@ -28,26 +46,45 @@ function Nav(props) {
   );
 }
 
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  )
-}
 
 function App() {
+  var [mode, setMode] = useState('WELCOME');
+  var [id, setId] = useState(null);
   const TOPICS = [
     {id: 1, title: 'html', body: 'html is ...'},
     {id: 2, title: 'css', body: 'css is ...'},
     {id: 3, title: 'js', body: 'js is ...'},
   ];
+  let content = null;
+
+  if (mode == 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if (mode == 'READ') {
+    let title, body = null;
+
+    // TOPICS.map((data, index) => {
+    //   if (data.id == id) {
+    //     title = data.title;
+    //     body = data.body;
+    //   }
+    // })
+
+    title = TOPICS[id - 1].title;
+    body = TOPICS[id - 1].body;
+
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav topics={TOPICS}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="REACT" onChangeMode={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={TOPICS} onChangeMode={(id) => {
+        setMode('READ');
+        setId(id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
